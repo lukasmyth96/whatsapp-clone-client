b7c73e10987e8f6141ecd3195ec82fb3c17aa893
lukasmyth96 <luka@nate.tech>
2020-09-12 12:10:45 +0100

    moved chat navbar to separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
+import ChatNavBar from './components/ChatNavBar';
 import ChatList from './components/ChatList';
 
 const App: React.FC = () => (
   <div>
-    <div>Whatsapp Clone</div>
+    <ChatNavBar />
     <ChatList />
   </div>
 );
---
+++ src/components/ChatNavBar.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ChatNavBar: React.FC = (props) => {
+  return <div>Whatsapp Clone</div>;
+};
+
+export default ChatNavBar;
