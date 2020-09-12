9ccb0d3688a00be60f1f25a7e26f21aa027ff010
lukasmyth96 <luka@nate.tech>
2020-09-12 17:56:53 +0100

    material UI toolbar for navbar

--- src/components/ChatListScreen/ChatNavBar.tsx
+++ src/components/ChatListScreen/ChatNavBar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { Toolbar } from '@material-ui/core';
 
-const ChatNavBar: React.FC = (props) => {
-  return <div>Whatsapp Clone</div>;
-};
+
+const ChatNavBar: React.FC = (props) => <Toolbar>Whatsapp Clone</Toolbar>;
 
 export default ChatNavBar;
