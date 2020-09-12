622a47de646b90045bead35fac2568a2883462c3
lukasmyth96 <luka@nate.tech>
2020-09-12 18:08:54 +0100

    added styles to each component using styled-components

--- src/components/ChatListScreen/ChatNavBar.tsx
+++ src/components/ChatListScreen/ChatNavBar.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { Toolbar } from '@material-ui/core';
+import styled from 'styled-components';
+
+const Container = styled(Toolbar)`
+  background-color: var(--primary-bg);
+  color: var(--primary-text);
+  font-size: 20px;
+  line-height: 40px;
+`;
 
 
-const ChatNavBar: React.FC = (props) => <Toolbar>Whatsapp Clone</Toolbar>;
+const ChatNavBar: React.FC = (props) => <Container>Whatsapp Clone</Container>;
 
 export default ChatNavBar;
--- src/components/ChatListScreen/index.tsx
+++ src/components/ChatListScreen/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import ChatNavBar from './ChatNavBar';
 import ChatList from './ChatList';
 
+const Container = styled.div`
+  height: 100vh;
+`;
+
 const ChatListScreen: React.FC = (props) => (
-  <div>
+  <Container>
     <ChatNavBar />
     <ChatList />
-  </div>
+  </Container>
 );
 
 export default ChatListScreen;
