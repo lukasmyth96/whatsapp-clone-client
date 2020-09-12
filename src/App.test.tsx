4d0367636ec79a1a23183e667642403596db9d99
lukasmyth96 <luka@nate.tech>
2020-09-12 11:40:01 +0100

    fixing test in app.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const { getByText } = render(<App name="test name" />);
+  const linkElement = getByText(/test name/i);
   expect(linkElement).toBeInTheDocument();
 });
