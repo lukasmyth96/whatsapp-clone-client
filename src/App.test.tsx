63886b8b2619382bcaf0cd9c57d8db3dc5ea8a57
lukasmyth96 <luka@nate.tech>
2020-09-12 12:08:08 +0100

    created ChatList component, removed name prop from App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App name="test name" />);
-  const linkElement = getByText(/test name/i);
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Whatsapp/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,11 @@
 import React from 'react';
 
-interface AppProps {
-  name?: string;
-}
+import ChatList from './components/ChatList';
 
-const App: React.FC<AppProps> = ({ name }) => (
+const App: React.FC = () => (
   <div>
     <div>Whatsapp Clone</div>
-    <div>
-      <ul>
-        <li>
-          <img
-            src="https://randomuser.me/api/portraits/thumb/men/1.jpg"
-            alt="Profile"
-          />
-          <div>Ethan Gonzalez</div>
-          <div>You on your way?</div>
-          <div>10:25</div>
-        </li>
-        <li>
-          <img
-            src="https://randomuser.me/api/portraits/thumb/men/2.jpg"
-            alt="Profile"
-          />
-          <div>Bryan Wallace</div>
-          <div>Hey, it's me</div>
-          <div>13:27</div>
-        </li>
-      </ul>
-    </div>
+    <ChatList />
   </div>
 );
 
